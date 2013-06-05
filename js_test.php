92800f8b88b8cef679d2de33eec41f0bee3acc9a
void-likeme <void-likeme@gmail.com>
2013-06-05 17:13:39 +0545

    testing

---
+++ js_test.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>
+		JS test
+	</title>
+<script type="text/javascript">
+	var p = window.location.pathname;
+	p = p.split('/');
+	document.write(p[1]);
+</script>
+</head>
+<body>
+</body>
+</html>
