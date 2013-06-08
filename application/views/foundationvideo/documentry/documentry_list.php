fbdec52f6c98463467514fff7ae5cf74827a28c6
void-likeme <void-likeme@gmail.com>
2013-06-08 23:28:42 +0545

    finished some designing on website
    suren can begin embedding videos on home page and others

---
+++ application/views/foundationvideo/documentry/documentry_list.php
@@ -0,0 +1,47 @@
+<h2>Documentry</h2>
+<div style="padding-left: 15px;">
+    <div class="box">
+	<div class="img"><a href="#"><img src="<?php echo base_url().'images/vol2.jpg'; ?>" alt="Image 3" width="206px" height="195px"/></div>
+	<div class="ttl"><a href="#">Documentry of CHOPEMA MAHA TIRTHAYATRA</a></div>
+    </div>
+    <div class="box">
+	<div class="img"><a href="#"><img src="<?php echo base_url().'images/vol8.jpg'; ?>" alt="Image 3" width="206px" height="195px"/></div>
+	<div class="ttl"><a href="#">Amrit Bachan</a></div>
+    </div>
+    <div class="box">
+	<div class="img"><a href="#"><img src="<?php echo base_url().'images/vol9.jpg'; ?>" alt="Image 3" width="206px" height="195px"/></div>
+	<div class="ttl"><a href="#">Prabachan</a></div>
+    </div>
+    <div class="box">
+	<div class="img"><a href="#"><img src="<?php echo base_url().'images/vol2.jpg'; ?>" alt="Image 3" width="206px" height="195px"/></div>
+	<div class="ttl"><a href="#">Documentry of CHOPEMA MAHA TIRTHAYATRA</a></div>
+    </div>
+    <div class="box">
+	<div class="img"><a href="#"><img src="<?php echo base_url().'images/vol8.jpg'; ?>" alt="Image 3" width="206px" height="195px"/></div>
+	<div class="ttl"><a href="#">Amrit Bachan</a></div>
+    </div>
+    <div class="box">
+	<div class="img"><a href="#"><img src="<?php echo base_url().'images/vol9.jpg'; ?>" alt="Image 3" width="206px" height="195px"/></div>
+	<div class="ttl"><a href="#">Prabachan</a></div>
+    </div>
+    <div class="box">
+	<div class="img"><a href="#"><img src="<?php echo base_url().'images/vol2.jpg'; ?>" alt="Image 3" width="206px" height="195px"/></div>
+	<div class="ttl"><a href="#">Documentry of CHOPEMA MAHA TIRTHAYATRA</a></div>
+    </div>
+    <div class="box">
+	<div class="img"><a href="#"><img src="<?php echo base_url().'images/vol8.jpg'; ?>" alt="Image 3" width="206px" height="195px"/></div>
+	<div class="ttl"><a href="#">Amrit Bachan</a></div>
+    </div>
+    <div class="box">
+	<div class="img"><a href="#"><img src="<?php echo base_url().'images/vol9.jpg'; ?>" alt="Image 3" width="206px" height="195px"/></div>
+	<div class="ttl"><a href="#">Prabachan</a></div>
+    </div>
+    <div class="box">
+	<div class="img"><a href="#"><img src="<?php echo base_url().'images/vol9.jpg'; ?>" alt="Image 3" width="206px" height="195px"/></div>
+	<div class="ttl"><a href="#">Prabachan</a></div>
+    </div>
+    <div class="box">
+	<div class="img"><a href="#"><img src="<?php echo base_url().'images/vol9.jpg'; ?>" alt="Image 3" width="206px" height="195px"/></div>
+	<div class="ttl"><a href="#">Prabachan</a></div>
+    </div>
+</div>
---
+++ application/views/general/general_lft.php
@@ -0,0 +1,8 @@
+<div id="foundation_lft_bdy">
+    <ul>
+        <li><a href="#teaching" class="box1" class="selected" class="box1">Teaching</a></li>
+        <li><a href="#work" class="box1">Work</a></li>
+        <li><a href="#biography" class="box1">Biography</a></li>
+        <li><a href="#documentry" class="box1">Documentry</a></li>
+    </ul>
+</div>
