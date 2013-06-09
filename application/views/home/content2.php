bde864f6fd6dc649f0575ebabe35939fee44bfef
Avinash <helloworld.avi@gmail.com>
2013-06-09 15:26:10 +0545

    link for foundation n general
    added from home page

--- application/views/home/content2.php
+++ application/views/home/content2.php
@@ -1,12 +1,12 @@
 <div class="col_w280 float_l">
                     <div class="title" align="center">Foundation Video</div>
-                    <div class="image_wrapper_02"><a href="<?php echo base_url().'watch/'.$videos['fvLatest'][1]['y_id']; ?>"><span><img src="images/foundation_video.jpg" alt="Image 2" height="120" width="280"/></span></a></div>
-                     Click here to watch recent foundation video.
+                    <div class="image_wrapper_02"><a href="<?php echo base_url().'foundation'; ?>"><span><img src="images/foundation_video.jpg" alt="Image 2" height="120" width="280"/></span></a></div>
+                     <a href="<?php echo base_url().'foundation'; ?>">Click here to watch foundation video.</a>
 </div>
 
 <div class="col_w280 float_l">
                     <div class="title ">General Video</div>
-                     <div class="image_wrapper_02"><a href="#"><span><img src="images/general_video.jpg" alt="Image 3" height="120" width="280"/></span></a></div>
+                     <div class="image_wrapper_02"><a href="<?php echo base_url().'general'; ?>"><span><img src="images/general_video.jpg" alt="Image 3" height="120" width="280"/></span></a></div>
                      <a href="#">Click here to watch recent video.</a>
 </div>
 
